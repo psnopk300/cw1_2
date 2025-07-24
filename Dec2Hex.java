1a9668ec189285acb73e169b81bcbe027c197df5
“psnopk300” <piotrsnopko@gmail.com>
2025-07-24 23:42:54 +0000

    HandleNoInt

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -7,7 +7,13 @@ class Dec2Hex {
             System.exit(1);
         }
 
-        Arg1 = Integer.parseInt(args[0]);
+        try {
+            Arg1 = Integer.parseInt(args[0]);
+        } catch (NumberFormatException e) {
+            System.err.println("Error: Invalid input. '" + args[0] + "' is not a valid integer.");
+            System.exit(1);
+        }
+
         char ch[] = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
         int rem, num;
         num = Arg1;
