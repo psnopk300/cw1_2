8185fcf8cb54a82766228c82c8cf4e4b263e99a5
“psnopk300” <piotrsnopko@gmail.com>
2025-07-24 22:32:24 +0000

    commit4

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -1,6 +1,5 @@
 
 import java.util.Scanner;
-
 class Dec2Hex
 {
 public static int Arg1;
@@ -19,7 +18,7 @@ public static int Arg1;
             num= num/16;
         }
 
-        System.out.println("Hexadecimal representation is: " + hexadecimal);
+        System.out.println("AHexadecimal representation is: " + hexadecimal);
 
     }
 }
