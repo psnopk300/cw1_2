b11ad2ff3bb7e0da7f18f34253e0c0d79998ffce
“psnopk300” <piotrsnopko@gmail.com>
2025-07-24 23:34:29 +0000

    HandleNoInput

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -1,25 +1,25 @@
+class Dec2Hex {
+    public static int Arg1;
 
-import java.util.Scanner;
-class Dec2Hex
-{
-public static int Arg1;
-    public static void main(String args[])    {
+    public static void main(String args[]) {
+        if (args.length == 0) {
+            System.err.println("Error: No input provided. Please provide an integer.");
+            System.exit(1);
+        }
+
         Arg1 = Integer.parseInt(args[0]);
-        char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+        char ch[] = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
         int rem, num;
         num = Arg1;
-        String hexadecimal="";
+        String hexadecimal = "";
         System.out.println("Converting the Decimal Value " + num + " to Hex...");
 
-        while(num != 0)
-        {
-            rem=num%16;
-            hexadecimal= ch[rem] + hexadecimal;
-            num= num/16;
+        while(num != 0) {
+            rem = num % 16;
+            hexadecimal = ch[rem] + hexadecimal;
+            num = num / 16;
         }
 
         System.out.println("Hexadecimal representation is: " + hexadecimal);
-
     }
 }
-
