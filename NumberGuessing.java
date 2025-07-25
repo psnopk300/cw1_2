9c64376616e6d2ff7a1decdcac6fff34ba8d135f
“psnopk300” <piotrsnopko@gmail.com>
2025-07-25 22:07:29 +0000

    “Sonarfix7”

--- NumberGuessing.java
+++ NumberGuessing.java
@@ -23,7 +23,7 @@ private static final Logger logger = Logger.getLogger(NumberGuessing.class.getNa
 
     // Game method which reads from scanner and returns number of attempts used
     public int play(Scanner sc) {
-    logger.info("A number is chosen between 1 and 100.");
+    logger.info(String.format("A number is chosen between 1 and 100."));
     logger.info("You have " + maxAttempts + " attempts to guess the correct number.");
 
     int attemptsUsed = 0;
