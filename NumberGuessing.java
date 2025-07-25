dafab2ed1703a13343dfd8a10a742e9cb2adb465
“psnopk300” <piotrsnopko@gmail.com>
2025-07-25 20:15:29 +0000

    “Sonarfix1”

--- NumberGuessing.java
+++ NumberGuessing.java
@@ -1,65 +1,58 @@
-package com.example.numberguessing;
-
+// NumberGuessing.java
 import java.util.Scanner;
-import java.util.Random;
-import java.util.logging.Logger;
+import java.util.logging.logger;
 
 public class NumberGuessing {
-
-    private static final Logger logger = Logger.getLogger(NumberGuessing.class.getName());
 
     private int number;
     private int maxAttempts;
 
     public NumberGuessing(int maxAttempts) {
         this.maxAttempts = maxAttempts;
-        Random rand = new Random();
-        this.number = 1 + rand.nextInt(100); // random number between 1 and 100
+        // random number between 1 and 100
+        this.number = 1 + (int)(100 * Math.random());
     }
 
-    // For testing: allow setting the number directly
+    // For testing, allow injecting number:
     public NumberGuessing(int maxAttempts, int number) {
         this.maxAttempts = maxAttempts;
         this.number = number;
     }
 
+    // Game method which reads from scanner and returns number of attempts used
     public int play(Scanner sc) {
-        logger.info("A number is chosen between 1 and 100.");
-        logger.info("You have " + maxAttempts + " attempts to guess the correct number.");
+    logger.info("A number is chosen between 1 and 100.");
+    System.out.println("You have " + maxAttempts + " attempts to guess the correct number.");
 
-        int attemptsUsed = 0;
+    int attemptsUsed = 0;
 
-        while (attemptsUsed < maxAttempts) {
-            logger.info("Enter your guess: ");
+    while (attemptsUsed < maxAttempts) {
+        System.out.print("Enter your guess: ");
 
-            if (!sc.hasNextInt()) {
-                logger.warning("Invalid input. Please enter an integer.");
-                if (sc.hasNext()) {
-                    sc.next();
-                } else {
-                    logger.warning("No more input. Ending game.");
-                    break;
-                }
-                continue;
-            }
-
-            int guess = sc.nextInt();
-            attemptsUsed++;
-
-            if (guess == number) {
-                logger.info("Congratulations! You guessed the correct number.");
-                return attemptsUsed;
-            } else if (guess < number) {
-                logger.info("The number is greater than " + guess);
-            } else {
-                logger.info("The number is less than " + guess);
-            }
+        if (!sc.hasNextInt()) {
+            System.out.println("Invalid input. Please enter an integer.");
+            sc.next();  // consume invalid input
+            continue;   // don't increase attemptsUsed
         }
 
-        logger.info("You've exhausted all attempts. The correct number was: " + number);
-        return attemptsUsed;
+        int guess = sc.nextInt();
+        attemptsUsed++;
+
+        if (guess == number) {
+            System.out.println("Congratulations! You guessed the correct number.");
+            return attemptsUsed;
+        } else if (guess < number) {
+            System.out.println("The number is greater than " + guess);
+        } else {
+            System.out.println("The number is less than " + guess);
+        }
     }
 
+    System.out.println("You've exhausted all attempts. The correct number was: " + number);
+    return attemptsUsed;
+}
+
+    // main method to run game normally
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         NumberGuessing game = new NumberGuessing(5);
