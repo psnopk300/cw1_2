4ebee1b09c47039c25033f1f9e23bd4f7dd4493a
“psnopk300” <piotrsnopko@gmail.com>
2025-07-25 01:25:33 +0000

    junitchange

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -1,31 +1,37 @@
-class Dec2Hex {
-    public static int Arg1;
+public class Dec2Hex {
 
-    public static void main(String args[]) {
-        if (args.length == 0) {
-            System.err.println("Error: No input provided. Please provide an integer.");
-            System.exit(1);
+    public String convert(int number) {
+        char[] ch = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+        String hexadecimal = "";
+
+        if (number == 0) {
+            return "0";
         }
 
-        try {
-            Arg1 = Integer.parseInt(args[0]);
-        } catch (NumberFormatException e) {
-            System.err.println("Error: Invalid input. '" + args[0] + "' is not a valid integer.");
+        while (number != 0) {
+            int rem = number % 16;
+            hexadecimal = ch[rem] + hexadecimal;
+            number = number / 16;
+        }
+
+        return hexadecimal;
+    }
+
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.err.println("Error: No input provided. Please provide an integer.");
             return;
         }
 
-        char ch[] = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
-        int rem, num;
-        num = Arg1;
-        String hexadecimal = "";
-        System.out.println("Converting the Decimal Value " + num + " to Hex...");
-
-        while(num != 0) {
-            rem = num % 16;
-            hexadecimal = ch[rem] + hexadecimal;
-            num = num / 16;
+        try {
+            int num = Integer.parseInt(args[0]);
+            System.out.println("Converting the Decimal Value " + num + " to Hex...");
+            Dec2Hex converter = new Dec2Hex();
+            String result = converter.convert(num);
+            System.out.println("Hexadecimal representation is: " + result);
+        } catch (NumberFormatException e) {
+            System.err.println("Error: Invalid input. '" + args[0] + "' is not a valid integer.");
         }
-
-        System.out.println("Hexadecimal representation is: " + hexadecimal);
     }
 }
+
